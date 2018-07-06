d44e738ca9f0444f860dbb398294b7904ecd639e
Rafał <rafal.poreba@solsoft.pl>
2018-07-06 09:22:13 +0200

    'd'

--- src/app/model.ts
+++ src/app/model.ts
@@ -1,11 +1,9 @@
-export class Recipe  {
-  public name: string;
-  public description: string;
-  public imagePath: string;
+export class Recipe {
+  constructor( public name: string, public description: string, public imagePath: string) {
+  }
+}
 
-  constructor( name: string,  desc: string,  imagePath: string) {
-    this.name = name;
-    this.description = desc;
-    this.imagePath = imagePath;
+export class Ingredient {
+  constructor(public name: string, public quantity: number) {
   }
 }
